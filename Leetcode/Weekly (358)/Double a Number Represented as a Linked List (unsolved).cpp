501611e80b5149be9e449c79b86d227547082009
Md Ashhar <mdashhar01@gmail.com>
2023-08-16 15:55:47 +0530

    Leetcode Weekly

---
+++ Leetcode/Weekly (358)/Double a Number Represented as a Linked List (unsolved).cpp
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* doubleIt(ListNode* head) {
+        ListNode* ptr = head;
+        if (ptr->val == 0)
+            return head;
+        else {
+            long long num = 0;
+            while (ptr != nullptr) {
+                int a = ptr->val;
+                num = num * 10 + a;
+                ptr = ptr->next;
+            }
+
+            num *= 2;
+            vector<int>digit;
+            while (num != 0) {
+                int ld = num % 10;
+                digit.push_back(ld);
+                num /= 10;
+            }
+
+            ListNode *new_node = new ListNode();
+            ListNode *dummyhead = new_node;
+            new_node->val = digit[digit.size() - 1];
+            for (int i = digit.size() - 2; i >= 0; i--) {
+                ListNode *node = new ListNode();
+                node->val = digit[i];
+                new_node->next = node;
+                new_node = node;
+            }
+
+            new_node->next = nullptr;
+
+            return dummyhead;
+        }
+    }
+};
