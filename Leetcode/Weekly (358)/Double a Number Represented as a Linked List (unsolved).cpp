77094b297c6dd87287901645a36ad6d419c41cd3
Md Ashhar <mdashhar01@gmail.com>
2023-08-16 19:57:37 +0530

    Weekly B (unsolved: Runtime error

--- Leetcode/Weekly (358)/Double a Number Represented as a Linked List (unsolved).cpp
+++ Leetcode/Weekly (358)/Double a Number Represented as a Linked List (unsolved).cpp
@@ -10,6 +10,13 @@
  */
 class Solution {
 public:
+
+    /* Getting runtime error due to overflow as no of nodes==10^4. And there are 10 numbers to be filled at
+       each node. Therefore the sum in worst case will be 9^10^4 >>> 10^18 (which is the max limit of long long)
+
+    */
+
+
     ListNode* doubleIt(ListNode* head) {
         ListNode* ptr = head;
         if (ptr->val == 0)
