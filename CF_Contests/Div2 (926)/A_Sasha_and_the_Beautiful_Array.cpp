cce859bbbf83d0508a5828f383b214fdf037a75c
Ashhar-24 <mdashhar01@gmail.com>
2024-02-16 18:42:32 +0530

    Optimised A (after contest)

--- CF_Contests/Div2 (926)/A_Sasha_and_the_Beautiful_Array.cpp
+++ CF_Contests/Div2 (926)/A_Sasha_and_the_Beautiful_Array.cpp
@@ -13,6 +13,11 @@ int main(){
 
         sort(arr.begin(), arr.end());
 
+        /*
+        Optimisation: Instead of Summation (a[1]-a[0]+a[2]-a[1]+....+a[n-1]-a[n-2]) which eventaully boils down to a[n-1]-a[0] ie, can be done in O(logn)
+        */
+
+
         int ans=0;
 
         for(int i=1; i<n; i++){
--- CF_Contests/Div2 (926)/C_Sasha_and_the_Casino.cpp
+++ CF_Contests/Div2 (926)/C_Sasha_and_the_Casino.cpp
@@ -4,9 +4,19 @@ using namespace std;
 int main(){
     int t; cin>>t;
     while(t--){
+        
+        // upsolve
+        /*
+            Logic: 
+            
+        */
+
+
         int bets, loss, coins;
         cin>>bets>>loss>>coins;
 
-        
+        if(bets*loss > coins) cout<<"NO"<<'\n';
+        else cout<<"YES"<<'\n';
+
     }
 }
