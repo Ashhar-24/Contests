13da3241a4b43546796800018e363b118eca6bb9
Ashhar-24 <mdashhar01@gmail.com>
2024-06-07 01:12:28 +0530

    Upsolve, Must revise Implementation

--- CF_Contests/Round 950 (Div. 3)/E_Permutation_of_Rows_and_Columns.cpp
+++ CF_Contests/Round 950 (Div. 3)/E_Permutation_of_Rows_and_Columns.cpp
@@ -1,21 +1,66 @@
 #include <bits/stdc++.h>
 #define ll long long
 using namespace std;
+const int N= 2e5+10;
 
 void solve(){
     int n,m; cin>>n>>m;
-    vector<vector<int>>a(n)(m);
-    vector<vector<int>>b(n)(m);
+    
+    // Unique way of implementation.
 
-    for(int i=0; i<n; i++){
-        for(int j=0; j<m; j++) cin>>a[i][j];
-    }
-    for(int i=0; i<n; i++){
-        for(int j=0; j<m; j++) cin>>b[i][j];
+    int row[N], col[N];         // keeping record of the initial position of the elements
+
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=m; j++){
+            int a; cin>>a;
+            row[a]=i;
+            col[a]=j;
+        }
     }
 
-    bool check=0;
-    if(n==1 || m==1)check=1;
+    // Taking the new array as input and matching the new position.
+
+    map<pair<int,int>, int> ctr;        // keeps record of the no of elements that change in posn from it i to i` (row)
+    map<pair<int,int>, int> ctc;        // keeps record of the no of elements that change in posn from it j to j` (col)
+
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=m; j++){
+            int a; cin>>a;
+            if(row[a]!=i){
+                ctr[{row[a], i}]++;
+            }
+
+            if(col[a]!=j){
+                ctc[{col[a], j}]++;
+            }
+        }
+    }
+
+    /*
+        The two maps stores the count of elements that changes it's position in row/col.
+
+        This is helpful as if a row is swapped to a new row then the total no of elements that have been moved to
+        new row (ie from r[a] to i) is equal to m (total cols). 
+        Similarly if a col is swapped to a new col, then no of elements swapped
+        from (col[a],j) will be equal to n (total rows).
+        By checking this we can find out that whether the new array is possible to obtain or not.
+    */ 
+
+    bool ans=1;
+
+    // Checking that the no of elements swapped from r[a] to i is equal to m (total cols) or not.
+    for(auto [i,j]: ctr){           // here [i,j] i represents {r[a],i} while j represent the total elements.
+        ans= ans & (j==m);
+    }
+
+    // Similarly for columns
+
+    for(auto [i,j]: ctc){
+        ans= ans & (j==n);
+    }
+
+    if(ans)cout<<"YES\n";
+    else cout<<"NO\n";
     
 }	
 
@@ -29,3 +74,5 @@ int main() {
 
     return 0;
 }
+
+// Upsolve
