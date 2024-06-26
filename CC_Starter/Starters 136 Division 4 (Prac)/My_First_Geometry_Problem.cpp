5496f953824913fe1ef44f3a8b1d084355744359
Ashhar-24 <mdashhar01@gmail.com>
2024-06-26 12:53:37 +0530

    contest_betterApproach (postcontest)

--- CC_Starter/Starters 136 Division 4 (Prac)/My_First_Geometry_Problem.cpp
+++ CC_Starter/Starters 136 Division 4 (Prac)/My_First_Geometry_Problem.cpp
@@ -4,26 +4,36 @@ using namespace std;
 
 void solve(){
     string s; cin>>s;
-    int ans=0, ct=0;
+    
+    // int ans=0, ct=0;
+    // for(int i=0; i<s.size(); i++){
+    //     if(s[i]=='1')ct++;
+    // }
+    // bool l=s[0]-'0', r=s[1]-'0', u=s[2]-'0', d=s[3]-'0';
+
+    // if (ct==1){
+    //     ans=11;
+    // }
+    // else if(ct==2){
+    //     if ((l and r) or (u and d))ans=21;
+    //     else ans=121;
+    // }
+    // else if(ct==3) ans=231;
+    // else{
+    //     ans=441;
+    // }
+
+    // cout<<ans<<'\n';
+
+    // Easier approach.
+
+    int leftright=1, updown=1;
     for(int i=0; i<s.size(); i++){
-        if(s[i]=='1')ct++;
-    }
-    bool l=s[0]-'0', r=s[1]-'0', u=s[2]-'0', d=s[3]-'0';
-
-    if (ct==1){
-        ans=11;
-    }
-    else if(ct==2){
-        if ((l and r) or (u and d))ans=21;
-        else ans=121;
-    }
-    else if(ct==3) ans=231;
-    else{
-        ans=441;
+        if(s[i]=='1' and i<2) leftright+=10;
+        if(s[i]=='1' and i>=2) updown+=10;
     }
 
-    cout<<ans<<'\n';
-
+    cout<<leftright*updown<<'\n';
 }	
 
 
