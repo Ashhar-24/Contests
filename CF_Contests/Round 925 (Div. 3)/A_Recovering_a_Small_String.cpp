5bc05fe9cd2b77e2ff44dafd51baa9c5bf87df5b
Ashhar-24 <mdashhar01@gmail.com>
2024-07-09 10:40:35 +0530

    uplsove_String

--- CF_Contests/Round 925 (Div. 3)/A_Recovering_a_Small_String.cpp
+++ CF_Contests/Round 925 (Div. 3)/A_Recovering_a_Small_String.cpp
@@ -1,81 +1,34 @@
-#include <iostream>
+#include <bits/stdc++.h>
+#define ll long long
 using namespace std;
 
-int main()
-{
-    int t;
-    cin >> t;
-    while (t--)
-    {
-        int n;
-        cin >> n;
-
-        /*
-            Approach (up-solve):
-            Set all letters as 'a' and decrease the vaalue of n by 3.
-            Traverse from ans[2] ie last element and increase until either it reaches 'z' or n==0. When it reaches 'z' and n!=0 do it for ans[1] and so on till arr[0].  
-        */
-
-        char ans[3];
-        for(int i=0; i<3; i++){
-            ans[i]='a';
-        }
-
-        n-=3;
-
-        for(int i=2; i>=0 && n>0; i--){
-            while(ans[i]!='z' && n>0){
-                n--;
-                ans[i]++;
+void solve(){
+    int n; cin>>n;
+    
+    for(int i=1; i<=26;i++){
+        for(int j=1; j<=26; j++){
+            for(int k=1; k<=26;k++){
+                if((i+j+k) == n){
+                    cout<<char('a'+i-1);
+                    cout<<char('a'+j-1);
+                    cout<<char('a'+k-1);
+                    cout<<endl;
+                    return;             // returning because there would be other pairs which would sum as 'n'
+                }
             }
         }
-
-        // string ans;
-
-        // int q = n / 26;
-        // int r = n % 26;
-
-        // if (q > 0 && n != 26)
-        // {
-        //     for (int i = 0; i < q; i++)
-        //     {
-        //         ans.append("z");
-        //     }
-        // }
-
-        // if (q > 0 && n != 26)
-        // {
-        //     for (int i = 0; i < q; i++)
-        //     {
-        //         ans[2 - i] = 'z';
-        //     }
-
-        //     if (3 - q == 1)
-        //         ans[0] = 'a' + r - 1;
-        //     else if (3 - q == 2)
-        //     {
-        //         ans[0] = 'a';
-        //         ans[1] = 'a' + r - 2;
-        //     }
-        // }
-
-        // else
-        // {
-
-        //     for (int i = 0; i < 2; i++)
-        //     {
-        //         ans[i] = 'a';
-        //     }
-
-        //     ans[2] = 'a' + r - 3;
-        // }
-
-        // printing
-        for (int i = 0; i < 3; i++)
-        {
-            cout << ans[i];
-        }
-
-        cout << '\n';
     }
+}	
+
+
+int main() {
+    int t=1; 
+    cin >> t;
+    while(t--){
+        solve();
+    }
+
+    return 0;
 }
+
+// upsolve :(
