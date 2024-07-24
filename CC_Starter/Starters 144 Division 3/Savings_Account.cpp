a9e859e920934f80ce395cbaaa78645e67748c03
Ashhar-24 <mdashhar01@gmail.com>
2024-07-24 22:21:22 +0530

    contest

---
+++ CC_Starter/Starters 144 Division 3/Savings_Account.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+#define int long long int
+#define pb push_back
+#define fore(i,a,b) for(int i = a; i < b; i++)
+#define revfore(i,a,b) for(int i = a; i >= b; i--)
+#define yes cout<<"YES\n"
+#define no cout<<"NO\n"
+using namespace std;
+
+bool isprime(int n){
+    if(n==1)return false;
+    for(int i=2; i<=sqrt(n); i++){
+        if(n%i==0)return false;
+    }
+    return true;
+}
+vector<int> convertbinary64(int n){
+    vector<int> v(64,0);
+    int i=0;
+    while(n>0){
+        v[i]=n%2;
+        n/=2;
+        i++;
+    }
+    return v;       // this represents the binary form in reverse manner
+}
+
+int convertdecimal(vector<int> v){
+    int ans=0;
+    for(int i=0; i<64; i++){
+        ans+=v[i]*pow(2,i);
+    }
+    return ans;
+}
+
+void solve(){
+    int x,y,z; cin>>x>>y>>z;
+    if(x*y==z){
+        cout<<0<<'\n'; return;
+    }
+    else{
+        int cnt=0;
+        while(x*y>z){
+            cnt++;
+            x--;
+        }
+        cout<<cnt<<'\n';
+    }
+    
+}	
+
+
+signed main() {
+    int t=1; 
+    cin >> t;
+    while(t--){
+        solve();
+    }
+
+    return 0;
+}
