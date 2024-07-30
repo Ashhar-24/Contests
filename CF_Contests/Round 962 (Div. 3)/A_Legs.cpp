906276a266400037b5d94a62e09aae918e937776
Ashhar-24 <mdashhar01@gmail.com>
2024-07-30 12:13:31 +0530

    contest

---
+++ CF_Contests/Round 962 (Div. 3)/A_Legs.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+#define int long long int
+#define pb push_back
+#define fore(i,a,b) for(int i = a; i < b; i++)
+#define revfore(i,a,b) for(int i = a; i >= b; i--)
+#define yes cout<<"YES\n"
+#define no cout<<"NO\n"
+#define INF 1e18
+using namespace std;
+
+bool isprime(int n){
+    if(n==1)return false;
+    for(int i=2; i<=sqrt(n); i++){
+        if(n%i==0)return false;
+    }
+    return true;
+}
+vector<int> convertbinary64(int n){
+    vector<int> v(64,0);
+    int i=0;
+    while(n>0){
+        v[i]=n%2;
+        n/=2;
+        i++;
+    }
+    return v;       // this represents the binary form in reverse manner
+}
+
+int convertdecimal(vector<int> v){
+    int ans=0;
+    for(int i=0; i<64; i++){
+        ans+=v[i]*pow(2,i);
+    }
+    return ans;
+}
+void solve() {
+    int n; cin >> n;
+    int cnt = 0;
+    // if (n == 2 || n == 4) {
+    //     cout << 1 << '\n'; return;
+    // }
+    // else {
+    //     while (n > 0) {
+    //         if (n >= 4) {
+    //             cnt++;
+    //             n -= 4;
+    //         }
+    //         else {
+    //             cnt++;
+    //             n -= 2;
+    //         }
+    //     }
+    // }
+    cnt = n/4;
+    cnt += (n%4)/2;
+
+    cout << cnt << '\n';
+}
+
+
+signed main() {
+    int t=1; 
+    cin >> t;
+    while(t--){
+        solve();
+    }
+
+    return 0;
+}
