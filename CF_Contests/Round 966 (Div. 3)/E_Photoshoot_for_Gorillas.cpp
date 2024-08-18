f6fbf3524c97bf92fef70643c422107268e36cbb
Ashhar-24 <mdashhar01@gmail.com>
2024-08-18 13:12:28 +0530

    upsolve_2DArray_Combinatorics*_Observation*

---
+++ CF_Contests/Round 966 (Div. 3)/E_Photoshoot_for_Gorillas.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define pb push_back
+#define fore(i,a,b) for(int i = a; i < b; i++)
+#define revfore(i,a,b) for(int i = a; i >= b; i--)
+#define all(v) v.begin(), v.end()
+#define yes cout<<"YES\n"
+#define no cout<<"NO\n"
+#define endl '\n'
+#define INF 1e18
+int expo(int a, int b, int mod) {int res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;}		// (a^b)%m
+bool isprime(int n){ if(n==1)return false; for(int i=2; i<=sqrt(n); i++){ if(n%i==0)return false;} return true;}
+vector<int> convertbinary64(int n){ vector<int> v(64,0); int i=0; while(n>0){ v[i]=n%2; n/=2; i++;} return v;}   // this represents the binary form in reverse manner
+int convertdecimal(vector<int> v){int ans=0; for(int i=0; i<64; i++){ ans+=v[i]*pow(2,i);}return ans;}
+
+void solve(){
+    int n,m,k; cin>>n>>m>>k;
+    int w; cin>>w;
+    vector<int>v(w);
+    fore(i,0,w)cin>>v[i];
+    
+    vector<int>contri;
+    fore(i,0,n){
+        fore(j,0,m){
+            int colstart= max(0LL,j-k+1);       // this tells the starting col of the subsquare such that (i,j)cell is covered 
+            int colend= min(j, m-k);          // this tells the ending col of the subsquare such that (i,j)cell is covered 
+            int rowstart= max(0LL, i-k+1);  // this tells the starting row of the subsquare such that (i,j)cell is covered 
+            int rowend= min(i, n-k);        // this tells the ending row of the subsquare such that (i,j)cell is covered 
+            
+            int temp = (colend-colstart+1) * (rowend-rowstart+1);   // the difference shows how many cols/rows would be covered
+                                                                    // the multiplication tells the no of subsquares in which (i,j)cell will be present
+            contri.pb(temp);
+        }
+    }
+    sort(all(contri));
+    reverse(all(contri));
+
+    sort(all(v));
+    reverse(all(v));
+
+    int ans=0;
+    for(int i=0; i<w; i++){
+        ans+= v[i]*contri[i];
+    }
+
+    cout<<ans<<endl;
+}	
+
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t=1; 
+    cin >> t;
+    while(t--){
+        solve();
+    }
+
+    return 0;
+}
+
+// upsolve
