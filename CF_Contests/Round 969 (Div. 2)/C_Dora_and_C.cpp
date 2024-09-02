908e7d2ae80ad9c528946105a1d04566c1211a74
Ashhar-24 <mdashhar01@gmail.com>
2024-09-02 22:17:28 +0530

    upsolve_Array_Maths(gcd)

---
+++ CF_Contests/Round 969 (Div. 2)/C_Dora_and_C.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define pb push_back
+#define fore(i,a,b) for(int i = a; i < b; i++)
+#define revfore(i,a,b) for(int i = a; i >= b; i--)
+#define all(v) v.begin(), v.end()
+#define yes cout<<"YES\n"
+#define no cout<<"NO\n"
+#define endl '\n'
+#define INF 1e18
+#define debug(x) cout<<x<<endl 
+int expo(int a, int b, int mod) {int res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;}		// (a^b)%m
+bool isprime(int n){ if(n==1)return false; for(int i=2; i<=sqrt(n); i++){ if(n%i==0)return false;} return true;}
+vector<int> convertbinary64(int n){ vector<int> v(64,0); int i=0; while(n>0){ v[i]=n%2; n/=2; i++;} return v;}   // this represents the binary form in reverse manner
+int convertdecimal(vector<int> v){int ans=0; for(int i=0; i<64; i++){ ans+=v[i]*pow(2,i);}return ans;}
+
+void solve(){
+    int n,a,b; cin>>n>>a>>b;
+    vector<int>v(n);
+    int g= __gcd(a,b);      // as this will result in smallest range in which no can be converted (works for a!=b)
+    fore(i,0,n){
+        cin>>v[i];
+        v[i]%=g;    // converting all by taking %g
+    }
+
+    sort(all(v));
+    int ans= v[n-1]-v[0];
+
+    fore(i,1,n){
+        ans=min(ans, v[i-1]+g-v[i]);        // as the previous element would now become the largest element by addition of g
+    }
+
+    cout<<ans<<endl;
+
+}	
+
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t=1; 
+    cin >> t;
+    while(t--){
+        solve();
+    }
+
+    return 0;
+}
+
+// upsolve
