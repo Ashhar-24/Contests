f785800b40b8d3fcb58716f918a61094f4885020
Ashhar-24 <mdashhar01@gmail.com>
2024-10-21 10:57:02 +0530

    upsolve_Greedy_Observation

---
+++ CF_Contests/Round 980 (Div. 2)/B_Buying_Lemonade.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define pb push_back
+#define fore(i,a,b) for(int i = a; i < b; i++)
+#define revfore(i,a,b) for(int i = a; i >= b; i--)
+#define all(v) v.begin(), v.end()
+#define yes cout<<"YES\n"
+#define no cout<<"NO\n"
+#define endl '\n'
+#define INF 1e18
+#define debug(x) cout<<x<<endl 
+// int expo(int a, int b, int mod) {int res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;}		// (a^b)%m
+// bool isprime(int n){ if(n==1)return false; for(int i=2; i<=sqrt(n); i++){ if(n%i==0)return false;} return true;}
+// vector<int> convertbinary64(int n){ vector<int> v(64,0); int i=0; while(n>0){ v[i]=n%2; n/=2; i++;} return v;}   // this represents the binary form in reverse manner
+// int convertdecimal(vector<int> v){int ans=0; for(int i=0; i<64; i++){ ans+=v[i]*pow(2,i);}return ans;}
+
+void solve(){
+    int n,k; cin>>n>>k;
+    vector<int>v(n);
+    fore(i,0,n)cin>>v[i];
+
+    sort(all(v));
+
+    int ans=k, subtract=0;      //  min k no of operations have to be performed
+                                // subtract keep track of the cans we have used till now
+
+    for(int i=0; i<n; i++){
+        v[i]-=subtract;
+        k-=min(k, (n-i)*v[i]);
+
+        if(k==0)break;
+        else ans++;
+
+        subtract+=v[i];
+    }
+
+    cout<<ans<<endl;
+}	
+
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t=1; 
+    cin >> t;
+    while(t--){
+        solve();
+    }
+
+    return 0;
+}
+
+// upsolve
